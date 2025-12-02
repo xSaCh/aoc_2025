357523154c59f592bf0f7146f1918b77b2bded64
xSaCh <samarthchauhan7@gmail.com>
2025-12-02 05:04:42 +0000

    day 1: part 2

--- day01/day01.go
+++ day01/day01.go
@@ -4,14 +4,15 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"math"
 	"os"
 	"strconv"
 )
 
 func main() {
-	f, _ := os.Open("./day01/input.txt")
+	f, _ := os.Open("./input.txt")
 	defer f.Close()
-	fmt.Println(part1(f))
+	fmt.Println(part2(f))
 }
 
 func part1(input io.Reader) int {
@@ -49,15 +50,26 @@ func part2(input io.Reader) int {
 		rotationsLine := sc.Text()
 		dir := rotationsLine[0]
 		distance, _ := strconv.Atoi(rotationsLine[1:])
-		switch dir {
-		case 'L':
-			curPoint = (curPoint - distance) % 100
-		case 'R':
-			curPoint = (curPoint + distance) % 100
-		}
-		if curPoint == 0 {
-			reachedZero++
+		for range distance {
+			switch dir {
+			case 'L':
+				curPoint = curPoint - 1
+			case 'R':
+				curPoint = curPoint + 1
+			}
+			curPoint = mod(curPoint)
+			if curPoint == 0 {
+				reachedZero++
+			}
 		}
 	}
 	return reachedZero
+}
+
+func mod(a int) int {
+	return (a%100 + 100) % 100
+}
+
+func quotient(a int) float64 {
+	return math.Floor(float64(a) / float64(100))
 }
--- day01/day01_test.go
+++ day01/day01_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math"
 	"strings"
 	"testing"
 
@@ -23,8 +24,22 @@ L82`)
 	require.Equal(t, 3, result)
 }
 func Test2(t *testing.T) {
-	input := strings.NewReader(``)
+	input := strings.NewReader(`L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82`)
 
 	result := part2(input)
-	require.Equal(t, 0, result)
+	require.Equal(t, 6, result)
+}
+
+func Test3(t *testing.T) {
+	result := int(math.Abs(quotient(50 + 1000)))
+	require.Equal(t, 10, result)
 }
